76df6a708b72af6637be57c1149452da1179adf8
Malati Desai <Malati Desai>
2025-05-11 13:55:25 +0530

    Some modifications

--- 01_Blue_Screen/build.bat
+++ 01_Blue_Screen/build.bat
@@ -2,11 +2,11 @@ cls
 
 del Source.exe
 
-cl.exe /c /EHsc /std:c++17 Source.cpp  -I "C:\VulkanSDK\1.3.204.1\Include"
+cl.exe /c /EHsc /std:c++17 Source.cpp  -I "C:\VulkanSDK\Vulkan\Include"
 
-cl.exe /c /EHsc /std:c++17 vk_engine.cpp  -I "C:\VulkanSDK\1.3.204.1\Include"
+cl.exe /c /EHsc /std:c++17 vk_engine.cpp  -I "C:\VulkanSDK\Vulkan\Include"
 
-link.exe Source.obj vk_engine.obj user32.lib gdi32.lib kernel32.lib "C:\VulkanSDK\1.3.204.1\Lib\vulkan-1.lib" /MACHINE:X64
+link.exe Source.obj vk_engine.obj user32.lib gdi32.lib kernel32.lib "C:\VulkanSDK\Vulkan\Lib\vulkan-1.lib" /MACHINE:X64
 
 del Source.obj vk_engine.obj
 
--- 03_Triangle_with_vbufferInput/build.bat
+++ 03_Triangle_with_vbufferInput/build.bat
@@ -2,14 +2,14 @@ cls
 
 del Source.exe
 
-C:\VulkanSDK\1.3.204.1\Bin\glslc.exe shaders\shader.vert -o shaders\vert.spv
-C:\VulkanSDK\1.3.204.1\Bin\glslc.exe shaders\shader.frag -o shaders\frag.spv
+C:\VulkanSDK\Vulkan\Bin\glslc.exe shaders\shader.vert -o shaders\vert.spv
+C:\VulkanSDK\Vulkan\Bin\glslc.exe shaders\shader.frag -o shaders\frag.spv
 
-cl.exe /c /EHsc /std:c++17 Source.cpp  -I "C:\VulkanSDK\1.3.204.1\Include"
+cl.exe /c /EHsc /std:c++17 Source.cpp  -I "C:\VulkanSDK\Vulkan\Include"
 
-cl.exe /c /EHsc /std:c++17 vk_engine.cpp  -I "C:\VulkanSDK\1.3.204.1\Include"
+cl.exe /c /EHsc /std:c++17 vk_engine.cpp  -I "C:\VulkanSDK\Vulkan\Include"
 
-link.exe Source.obj vk_engine.obj user32.lib gdi32.lib kernel32.lib "C:\VulkanSDK\1.3.204.1\Lib\vulkan-1.lib" /MACHINE:X64
+link.exe Source.obj vk_engine.obj user32.lib gdi32.lib kernel32.lib "C:\VulkanSDK\Vulkan\Lib\vulkan-1.lib" /MACHINE:X64
 
 del Source.obj vk_engine.obj
 
